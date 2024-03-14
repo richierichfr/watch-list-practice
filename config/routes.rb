c0b930a865f57432364efd194d68d79aebcf16fc
ulrich aguey <ulrichatlanta@hotmail.com>
2024-03-14 19:00:22 +0100

    beginning of the models

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
+  # get "up" => "rails/health#show", as: :rails_health_check
+  get "lists", to: "lists#index"
+  get "lists/new", to: "lists#new"
+  post "lists", to: "lists#create"
+  get "lists/id", to: "lists#show"
   # Defines the root path route ("/")
   # root "posts#index"
 end
