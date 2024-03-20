5226e2dc1874f1af46536a7c3153b1fb234a267c
ulrich aguey <ulrichatlanta@hotmail.com>
2024-03-20 18:45:40 +0100

    all routes

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,19 +2,23 @@ class BookmarksController < ApplicationController
 
 
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmarks_params)
     @bookmark.list = @list
-    @bookmark.save
-    redirect_to list_path(@list)
+  if @bookmark.save
+      redirect_to list_path(@list)
+
+    end
   end
 
   private
 
   def bookmarks_params
-    params.require(:bookmark).permit[:comment]
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  validates :title, :overview, presence: true
+  validates :title, presence: true, uniqueness:true
+  validates :overview, presence: true
 end
